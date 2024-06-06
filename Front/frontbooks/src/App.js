a620b209581d63786e69efbaeef88de20eca6161
Milk_ <lonknoodle@gmail.com>
2024-06-06 23:54:07 +0300

    front setup

---
+++ Front/frontbooks/src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  
+  
+  
+  
+  
+  return (
+    <UserProvider>
+      <Router>
+        <Route path="/">{<Home/>}</Route>
+        <Route path="/">{}</Route>
+        <Route path="/">{}</Route>
+        <Route path="/">{}</Route>
+        <Route path="/">{}</Route>
+      </Router>
+
+    </UserProvider>
+  )
+}
+
+export default App;
