0fc77ffe79133513983234aa715633aba98296f2
LauraC360 <laurachiriac.cl@gmail.com>
2024-06-07 04:13:16 +0300

    updated-spring-app

---
+++ BookRec_JAVA/project/src/main/java/com/example/project/authentication/user/User.java
@@ -0,0 +1,23 @@
+package com.example.project.authentication.user;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    private String password;
+
+    private boolean enabled;
+
+    // Getters and setters
+}
